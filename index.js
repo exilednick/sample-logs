9af117c49d9420564cd55c123086aa437aa48c62
Nikhil Jha <exilednick@outlook.com>
2021-08-26 16:03:08 +0530

    added routes

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/err', async(req, res) => {
+    console.log("ERROR: this is sample error")
+    res.json({
+        err: "sample"
+    })
+})
+
+app.get('/info', async(req, res) => {
+    console.log("INFO: this is sample info")
+})
+
+app.listen(5000, () => console.log("INFO: server started"))
